89b0f464b7e6f1bb2bcd0853ab8688bf10c08110
sajanIocod <sajan@iocod.com>
2024-08-28 15:37:37 +0530

    fix : added documentation

--- lib/src/sms_segment_calculator.dart
+++ lib/src/sms_segment_calculator.dart
@@ -1,11 +1,37 @@
 import '../message_segment_calculator.dart'; // Ensure this path matches where your encoding maps are defined.
 
+/// A calculator for determining the number of SMS segments required for a given message.
+/// This class provides tools to account for both standard GSM encoding and UCS-2 encoding
+/// when special characters or emojis are present in the message.
 class SMSegmentCalculator {
+  /// The maximum number of characters allowed in a single SMS segment using GSM encoding.
   static const int GSM_SINGLE_SEGMENT_LIMIT = 160;
+
+  /// The maximum number of characters allowed in a single SMS segment using UCS-2 encoding.
   static const int UCS2_SINGLE_SEGMENT_LIMIT = 70;
+
+  /// The maximum number of characters in a concatenated SMS segment using GSM encoding.
+  /// Concatenated messages are used when the message exceeds the limit for a single SMS
+  /// and need to be split into multiple segments.
   static const int CONCATENATED_GSM_SEGMENT_LIMIT = 153;
+
+  /// The maximum number of characters in a concatenated SMS segment using UCS-2 encoding.
+  /// This limit accounts for the additional header information required to piece the segments
+  /// together in the correct order when the message is received.
   static const int CONCATENATED_UCS2_SEGMENT_LIMIT = 67;
 
+  /// Calculates the number of segments required to send a given message and the total
+  /// character count of the message, considering whether it requires GSM or UCS-2 encoding.
+  ///
+  /// The function checks each character of the message to determine the appropriate encoding
+  /// and then calculates the segment count based on the message length and encoding requirements.
+  ///
+  /// Returns a tuple containing the total number of segments and the character count.
+  ///
+  /// [message] - The SMS message for which segments are being calculated.
+  /// Returns a [Tuple] where:
+  /// - `item1` is the number of segments.
+  /// - `item2` is the total character count.
   static Tuple2<int, int> calculateSegments(String message) {
     bool usesUCS2 = message.runes.any((int rune) {
       var character = String.fromCharCode(rune);
