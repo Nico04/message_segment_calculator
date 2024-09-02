4e0d1ba9b28cc9092175a7b85f5b3486c6db9f6b
sajanIocod <sajan@iocod.com>
2024-09-02 14:03:39 +0530

    fix : add documentation

--- lib/src/segments.dart
+++ lib/src/segments.dart
@@ -4,20 +4,20 @@ import 'user_data_header.dart'; // Importing the UserDataHeader class.
 /// Segment Class
 /// A wrapper around List to represent one segment and add some helper functions
 class Segment {
-  // Explicitly typing _elements as List<dynamic> for better type safety and clarity.
+  /// Explicitly typing _elements as List<dynamic> for better type safety and clarity.
   final List<dynamic> _elements = [];
 
-  // Boolean to indicate if Twilio reserved bits are used.
+  /// Boolean to indicate if Twilio reserved bits are used.
   bool hasTwilioReservedBits;
 
-  // Boolean to indicate if a user data header is present in the segment.
+  /// Boolean to indicate if a user data header is present in the segment.
   bool hasUserDataHeader;
 
-  // Constructor with optional parameter to initialize with user data headers.
+  /// Constructor with optional parameter to initialize with user data headers.
   Segment({bool withUserDataHeader = false})
       : hasTwilioReservedBits = withUserDataHeader,
         hasUserDataHeader = withUserDataHeader {
-    // If initialized with user data header, add 6 user data header objects to _elements.
+    /// If initialized with user data header, add 6 user data header objects to _elements.
     if (withUserDataHeader) {
       for (int i = 0; i < 6; i++) {
         _elements.add(UserDataHeader());
@@ -25,10 +25,10 @@ class Segment {
     }
   }
 
-  // Getter to provide access to _elements as an unmodifiable list for safety.
+  /// Getter to provide access to _elements as an unmodifiable list for safety.
   List<dynamic> get elements => List.unmodifiable(_elements);
 
-  // Computes the total size in bits of the segment, including user data headers if present.
+  /// Computes the total size in bits of the segment, including user data headers if present.
   int sizeInBits() {
     return _elements.fold(
         0,
@@ -39,32 +39,36 @@ class Segment {
                 : (el as UserDataHeader).sizeInBits()));
   }
 
-  // Computes the size in bits of the message content only, excluding user data headers.
+  /// Computes the size in bits of the message content only, excluding user data headers.
   int messageSizeInBits() => _elements
       .whereType<EncodedChar>()
       .fold(0, (int total, EncodedChar el) => total + el.sizeInBits());
 
-  // Calculates the remaining free space in bits within this segment.
+  /// Calculates the remaining free space in bits within this segment.
   int freeSizeInBits() {
     const int maxBitsInSegment =
         1120; // Maximum size of an SMS is 140 octets -> 140 * 8 bits = 1120 bits
     return maxBitsInSegment - sizeInBits();
   }
 
-  // Adds headers if they are not already present and manages overflow of characters.
+  /// Adds headers if they are not already present and manages overflow of characters.
   List<EncodedChar> addHeader() {
     if (hasUserDataHeader) {
-      return []; // Return an empty list if headers are already present.
+      return [];
+
+      /// Return an empty list if headers are already present.
     }
     List<EncodedChar> leftOverChar = [];
-    hasTwilioReservedBits =
-        true; // Indicate that Twilio reserved bits are used.
+    hasTwilioReservedBits = true;
+
+    /// Indicate that Twilio reserved bits are used.
     hasUserDataHeader = true; // Indicate that a user data header is now added.
     for (int i = 0; i < 6; i++) {
-      // Add 6 user data headers at the start of the segment.
+      /// Add 6 user data headers at the start of the segment.
       _elements.insert(0, UserDataHeader());
     }
-    // Remove elements until there is enough free space in the segment.
+
+    /// Remove elements until there is enough free space in the segment.
     while (freeSizeInBits() < 0 && _elements.isNotEmpty) {
       var removed = _elements.removeLast();
       if (removed is EncodedChar) {
@@ -75,12 +79,12 @@ class Segment {
     return leftOverChar; // Return any leftover characters that could not fit after adding headers.
   }
 
-  // Adds an element (either EncodedChar or UserDataHeader) to the segment.
+  /// Adds an element (either EncodedChar or UserDataHeader) to the segment.
   void add(dynamic element) {
     _elements.add(element);
   }
 
-  // Removes and returns the last element from the segment.
+  /// Removes and returns the last element from the segment.
   dynamic removeLast() {
     return _elements.removeLast();
   }
