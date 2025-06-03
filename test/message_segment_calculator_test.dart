ab607e824b32da46567aa3bc67f8e3557bc7dab4
Nico04 <34476051+Nico04@users.noreply.github.com>
2025-06-03 12:15:40 +0200

    add tests

--- test/message_segment_calculator_test.dart
+++ test/message_segment_calculator_test.dart
@@ -6,7 +6,7 @@ import 'package:message_segment_calculator/src/segment.dart';
 void main() {
   group('SegmentedMessage Tests', () {
     test('Calculates correct segments for a simple GSM-7 message', () {
-      String message = 'Hello, this is a test message!';
+      const message = 'Hello, this is a test message!';
       SegmentedMessage segmentedMessage = SegmentedMessage(message);
 
       expect(segmentedMessage.segmentsCount, 1);
@@ -14,8 +14,26 @@ void main() {
       expect(segmentedMessage.encoding, SmsEncoding.gsm7);
     });
 
+    test('Calculates correct segments for a long GSM-7 message', () {
+      const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolor';
+      SegmentedMessage segmentedMessage = SegmentedMessage(message);
+
+      expect(segmentedMessage.segmentsCount, 3);
+      expect(segmentedMessage.totalSize, lessThanOrEqualTo(2293)); // 1 segment
+      expect(segmentedMessage.encoding, SmsEncoding.gsm7);
+    });
+
+    test('Calculates correct segments for a long UCS-2 message', () {
+      const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolorç';
+      SegmentedMessage segmentedMessage = SegmentedMessage(message);
+
+      expect(segmentedMessage.segmentsCount, 5);
+      expect(segmentedMessage.totalSize, lessThanOrEqualTo(5168)); // 1 segment
+      expect(segmentedMessage.encoding, SmsEncoding.ucs2);
+    });
+
     test('Calculates correct segments for a message with emojis', () {
-      String message = 'Hello 😊! This message contains emojis.';
+      const message = 'Hello 😊! This message contains emojis.';
       SegmentedMessage segmentedMessage = SegmentedMessage(message);
 
       expect(segmentedMessage.segmentsCount, greaterThanOrEqualTo(1));
@@ -23,7 +41,7 @@ void main() {
     });
 
     test('Handles UCS-2 encoding for special characters', () {
-      String message = 'こんにちは世界'; // Japanese for "Hello, World"
+      const message = 'こんにちは世界'; // Japanese for "Hello, World"
       SegmentedMessage segmentedMessage = SegmentedMessage(message);
 
       expect(segmentedMessage.encoding, SmsEncoding.ucs2);
@@ -31,7 +49,7 @@ void main() {
     });
 
     test('Throws exception for incompatible GSM-7 characters', () {
-      String message = 'Hello 😊!';
+      const message = 'Hello 😊!';
 
       expect(
         () => SegmentedMessage(message, SmsEncodingMode.gsm7),
@@ -40,7 +58,7 @@ void main() {
     });
 
     test('Detects line break styles correctly', () {
-      String message = 'Hello\nWorld\r\nNew Line';
+      const message = 'Hello\nWorld\r\nNew Line';
 
       SegmentedMessage segmentedMessage = SegmentedMessage(message);
 
@@ -48,7 +66,7 @@ void main() {
     });
 
     test('Identifies non-GSM characters', () {
-      String message = 'Hello 😊!';
+      const message = 'Hello 😊!';
 
       SegmentedMessage segmentedMessage = SegmentedMessage(message);
       List<String?> nonGsmChars = segmentedMessage.getNonGsmCharacters();
@@ -59,7 +77,7 @@ void main() {
 
   group('EncodedChar Tests', () {
     test('Creates EncodedChar correctly for GSM-7 character', () {
-      EncodedChar encodedChar = EncodedChar('A', SmsEncoding.gsm7);
+      final encodedChar = EncodedChar('A', SmsEncoding.gsm7);
 
       expect(encodedChar.isGSM7, true);
       expect(encodedChar.codeUnits?.length, 1);
@@ -67,7 +85,7 @@ void main() {
     });
 
     test('Creates EncodedChar correctly for UCS-2 character', () {
-      EncodedChar encodedChar = EncodedChar('😊', SmsEncoding.ucs2);
+      final encodedChar = EncodedChar('😊', SmsEncoding.ucs2);
 
       expect(encodedChar.isGSM7, false);
       expect(encodedChar.codeUnits?.length, greaterThanOrEqualTo(1));
@@ -77,18 +95,18 @@ void main() {
 
   group('Segment Tests', () {
     test('Calculates free size correctly after adding characters', () {
-      Segment segment = Segment();
-      EncodedChar char = EncodedChar('A', SmsEncoding.gsm7);
+      final segment = Segment();
+      final char = EncodedChar('A', SmsEncoding.gsm7);
       segment.add(char);
 
       expect(segment.freeSizeInBits(), lessThan(1120));
     });
 
     test('Adds headers correctly and manages overflow', () {
-      Segment segment = Segment();
-      EncodedChar char = EncodedChar('A', SmsEncoding.gsm7);
+      final segment = Segment();
+      final char = EncodedChar('A', SmsEncoding.gsm7);
       segment.add(char);
-      List<EncodedChar> overflow = segment.addHeader();
+      final overflow = segment.addHeader();
 
       expect(segment.hasUserDataHeader, true);
       expect(segment.elements.length, greaterThan(1)); // Includes headers
@@ -98,7 +116,7 @@ void main() {
 
   group('UserDataHeader Tests', () {
     test('Calculates size in bits correctly for UserDataHeader', () {
-      UserDataHeader header = UserDataHeader();
+      final header = UserDataHeader();
 
       expect(header.sizeInBits(), 8);
     });
